9c5da4b48e85f19c569565264ded8328d5372790
Tiffany Areco <tj.areco.e@gmail.com>
2022-02-02 16:56:33 +0100

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "belgian",phone_number: "12345678" }
+pizza_east = { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian" ,phone_number: "12345678" }
+five_guys = {name: "Five Guys", address: "La Defense", category: "italian", phone_number: "12345678"}
+surpriz = { name: "Sürpriz", address: "Rue Oberkampf", category: "belgian", phone_number: "12345678"}
+origami = {name: "Origami", address: "Rue Alesia", category: "japanese", phone_number: "12345678"}
+
+[dishoom, pizza_east, five_guys, surpriz, origami].each do |attributes|
+  restaurant = Restaurant.create(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
